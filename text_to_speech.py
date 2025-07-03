7b703363073dbead0b79bd460c7731e88bed8ea1
Arpit-Padmani <arpitpadmani197@gmail.com>
2025-07-03 23:56:39 +0530

    try to add gemini and many more ai like transformar openrouter ai and many more but not added  and change in all speech reconigtaion add valiadtion on it also add in parsing from voice and wokring on add validation on adding meeting

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,12 +1,12 @@
 # text_to_speech.py
 import pyttsx3
 
-def speak_text(text: str):
+def speak_text(text):
     engine = pyttsx3.init()
     rate = engine.getProperty('rate')
     engine.setProperty('rate', rate - 50)
     engine.setProperty('volume', 1)
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('voice', voices[-1].id)
     engine.say(text)
     engine.runAndWait()
